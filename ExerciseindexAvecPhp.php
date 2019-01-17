4437f9d841c280e4f7c7155c51d761a4f6d450b5
wx19999 <46788157+wx19999@users.noreply.github.com>
2019-01-17 14:28:05 -0500

    Add files via upload

---
+++ ExerciseindexAvecPhp.php
@@ -0,0 +1,32 @@
+<Doctype HTML>
+<HTML>
+<head>
+<nav>
+<ul>
+<li>Accueil</li>
+<li>Securite</li> 
+<li>Test</li>
+</nav>
+<title> MonSite.com </title>
+<meta charset = "UTF-8">
+</head>
+
+<h1> Bienvenue à mon site<br> </h1>
+<?php
+echo "Bienvenue à php<br>";
+echo "<marquee>Continuer avec HTML</marquee>";
+echo "Le abonnées pour cet hiver";
+?>
+<section>
+Permier titre
+<article> 
+<p> testlllllll </p>
+</section>
+<section>
+Deuxième titre
+<p> testllllll</p>
+</section>
+<aside> hello </aside>
+<footer> end </footer>
+
+</head>
